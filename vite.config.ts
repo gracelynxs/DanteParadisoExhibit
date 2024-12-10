36af4f301738cdb4bf81567196da6dece4839449
Gracelyn <gracelyn.shi@gmail.com>
2024-12-09 23:02:37 -0600

    setup

--- vite.config.ts
+++ vite.config.ts
@@ -3,16 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: 'DanteParadisoExhibit',
-  build: {
-    outDir: 'dist',
-    rollupOptions: {
-      input:{
-        main: 'src/App.tsx',
-        scene: 'src/Scene.tsx'
-      },      
-      external: ['react-router-dom']
-    }
-  }
-
+  base: '/DanteParadisoExhibit/'
 })
